315fe57e251556cbe3232eb9d401f73478baacfc
Simone <simonedambrgio@live.it>
2021-03-12 17:35:34 +0100

    upload material

---
+++ 2021.03.12 Slides/R/Script.R
@@ -0,0 +1,28 @@
+read.table(file = "Data/earlymath.txt", header = TRUE)
+
+
+1 + 1 
+
+library(xlsx)
+
+install.packages("xlsx")
+
+
+library(foreign)
+
+
+#Altre operazioni
+
+dati_csv <- read.csv("Data/earlymath.csv")
+
+dati_csv <- read.csv("../earlymath.csv")
+
+dati_txt <- read.table(file = "Data/earlymath.txt", header = TRUE)
+
+file.choose()
+
+read.csv("/home/simone/Dropbox/Ongoing Projects/Didattica Integrativa/2021.03.12 Slides/R/Data/earlymath.csv")
+
+getwd()
+
+load("Data/earlymath.rda")
